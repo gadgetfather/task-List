bc4d524725f399c501bf8c716fac7aa8eb2d16f5
Akshay Kamble <kamblea635@gmail.com>
2021-12-10 15:55:48 +0530

    selected elements

---
+++ app.js
@@ -0,0 +1,8 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input")
+const todoBtn = document.querySelector(".todo-button")
+const todoList = document.querySelector(".todo-list")
+//Event Listener
+
+
+// Functions
